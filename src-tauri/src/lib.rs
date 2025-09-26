7e02c38f39602d52be30a46a77955a07e4b82116
Zephrnos <47162200+Zephrnos@users.noreply.github.com>
2025-09-26 16:29:55 -0500

    Made a working GUI for the first time
    Made a working GUI for the first time to understand Tauri. Created image.rs to make future datatypes.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,12 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![get_user])
+    .invoke_handler(tauri::generate_handler![my_custom_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn get_user() {
-  println!("Get User");
+fn my_custom_command() {
+  println!("I was invoked from JavaScript!");
 }
