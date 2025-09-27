925c47c2dac32bf528a99ab2fe2a1fc612aa9850
Zephrnos <47162200+Zephrnos@users.noreply.github.com>
2025-09-27 01:20:41 -0500

    Implement initial crop_image logic in cropper.rs
    Refactored crop_image to accept DynamicImage and PaintingSize, and added logic to calculate crop dimensions and positions. The function now iterates over crop sizes and applies cropping, though the implementation is incomplete and still contains a todo.

--- src-tauri/src/core/cropper.rs
+++ src-tauri/src/core/cropper.rs
@@ -1,6 +1,30 @@
-use crate::models::painting::Painting;
+use crate::models::{painting::{self, Painting}, painting_size::PaintingSize};
+use image::{DynamicImage, GenericImageView, imageops::crop};
 
-pub fn crop_image(image: Painting) -> Painting {
-    todo!()
+pub fn crop_image(image: DynamicImage, crop_data: PaintingSize) -> Vec<Painting> {
+
+    let paintings: Vec<Painting> = Vec::new();
     
+    let crops = crop_data.get_size();
+
+    let (width, height) = image.dimensions();
+    let (ratio_x, ratio_y) = &crops[0];
+
+    let magic_number = match 
+      (ratio_x / ratio_y) >= (width / height) {
+        true => width / ratio_x,
+        false => height / ratio_y,
+    };
+
+    let crop_width = ratio_x * magic_number;
+    let crop_height = ratio_y * magic_number;
+    let width_start = (width - crop_width) / 2;
+    let height_start = (height - crop_height) / 2;
+
+    
+    for crop_size in crops{
+        crop(&mut image.clone(), width_start, height_start, crop_width, crop_height);
+    }
+
+    todo!()
 }
